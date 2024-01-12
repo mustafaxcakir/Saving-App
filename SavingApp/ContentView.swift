b38c718d12027535eee48d6e71b01d17c5abda76
Mustafa Cakir <144024304+MxCann@users.noreply.github.com>
2024-01-12 12:32:31 +0300

    Initial Commit

---
+++ SavingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SavingApp
+//
+//  Created by Mustafa Çakır on 12.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SavingApp/SavingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavingAppApp.swift
+//  SavingApp
+//
+//  Created by Mustafa Çakır on 12.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SavingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
